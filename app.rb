5c525c7cde0309f4d565c826e11bbbc2829b7a04
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-12-19 11:03:35 +0000

    feat: added Campaign class & .view_all_campaigns & include call in app.rb

--- app.rb
+++ app.rb
@@ -1,15 +1,18 @@
 require 'sinatra/base'
+require './app/model/campaigns'
+require './app/model/database'
 
 class Seedlings < Sinatra::Base
 
   get '/' do
     'Hello World!'
+    @campaigns = Campaign.view_all_Campaigns
     erb :campaign_list
   end
 
   configure do
-    set :views, Proc.new { File.join(root, "app/views") }
-    enable :sessions
+      set :views, Proc.new { File.join(root, "app/views") }
+      enable :sessions
   end
 
 end
---
+++ app/model/campaigns.rb
@@ -0,0 +1,24 @@
+require 'pg'
+
+class Campaign
+
+  attr_reader :name, :image, :country, :sector, :target_amount
+
+  def initialize(name:, image:, country:, sector:, target_amount:)
+    @name = name
+    @image = image
+    @country = country
+    @sector = sector
+    @target_amount = target_amount
+  end
+
+  def self.view_all_Campaigns
+    Database.setup_connection
+    campaigns = Database.run_query('SELECT * FROM campaigns;')
+
+    campaigns.map do |campaign|
+      Campaign.new(name: campaign["name"], image: campaign["image"], country: campaign["country"], sector: campaign["sector"], target_amount: campaign["target_amount"])
+    end
+  end
+
+end
