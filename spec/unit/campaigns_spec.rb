f785cc5e7b7d79d2bfe1911279824d2a04cfcb32
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-12-19 10:07:21 +0000

    test: added unit test for Campaign.view_all_campaigns

---
+++ spec/unit/campaigns_spec.rb
@@ -0,0 +1,16 @@
+require './app/model/campaign'
+
+describe Campaign do
+  describe 'view_all_campaigns' do
+    it 'should display all campaigns' do
+      visit ('/')
+      expect(page).to have_title('Campaign List')
+      expect(page).to have_content('Campaign_1')
+      expect(page).to have_content('Campaign_2')
+      expect(page).to have_content('Campaign_3')
+      expect(page).to have_content('Campaign_4')
+      expect(page).to have_content('Campaign_5')
+      expect(page).to have_content('Campaign_20')
+    end
+  end
+end
