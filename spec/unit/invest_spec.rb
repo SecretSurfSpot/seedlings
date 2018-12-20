db595e456f8719077e7e22e283ca1e878eee1bc0
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-12-20 14:54:41 +0000

    test: added Invest.invest_in_campaign feature & unit tests

---
+++ spec/unit/invest_spec.rb
@@ -0,0 +1,10 @@
+# require './app/model/invest'
+#
+# describe Invest do
+#   describe '.invest_in_campaign' do
+#     it 'should invest the given amount into the chosen campaign' do
+#       investment = Invest.invest_in_campaign(name: 'Campaign_3', amount: 10000)
+#       expect(investment[0]).to include({ "amount" => "10000", "campaign_name" => "Campaign_3" })
+#     end
+#   end
+# end
