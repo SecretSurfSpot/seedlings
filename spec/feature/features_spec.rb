a8b8d169f8effb767f0a7042b07e7e84f48ee934
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-12-20 18:23:26 +0000

    refactor: modifications in line with Rubocop

--- spec/feature/features_spec.rb
+++ spec/feature/features_spec.rb
@@ -25,10 +25,10 @@ feature 'Feature Tests' do
   scenario 'User invests £65000 into Campaign_3' do
     visit '/'
     find(:xpath, "//a[@href='/Campaign_3']").click
-    fill_in 'amount', :with => '65000'
+    fill_in 'amount', with: '65000'
     find_button('Invest').click
     expect(page).to have_content('10% Funded')
-    # expect(page).to have_content('Congratulation! You have just invested £10000 in Campaign_3')
+    # expect(page).to have_content('Congratulation! You have just invested 10000 in Campaign_3')
     # expect(page).to have_selector(:link_or_button, '/')
   end
 end
