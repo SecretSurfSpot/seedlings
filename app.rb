4a19f491378cbcc603864889f58db81cd4dcb1bb
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-01-16 15:07:50 +0000

    refactor: enabled sessions, modified post '/invest' & confirmation.erb to use sessions

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require './app/model/invest'
 require './app/model/database'
 
 class Seedlings < Sinatra::Base
+  enable :sessions
+
   get '/' do
     @all_campaigns = Campaign.view_all_campaigns
     erb :campaign_list
@@ -15,7 +17,11 @@ class Seedlings < Sinatra::Base
   end
 
   post '/invest' do
-    @investment = Invest.invest_in_campaign(name: params[:campaign_name], amount: params[:amount])
+    Invest.invest_in_campaign(name: params[:campaign_name], amount: params[:amount])
+    session[:campaign_name] = params[:campaign_name]
+    session[:amount] = params[:amount]
+    p "1 param campaign_name is #{params[:campaign_name]}"
+    p "2 session campaign_name is #{session[:campaign_name]}"
     erb :confirmation
   end
 
