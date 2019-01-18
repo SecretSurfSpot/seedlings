1011c651268f97adda77df32d3c7f88aaa6386dc
Andrew Gibbs <asg1968@hotmail.co.uk>
2019-01-18 12:48:54 +0000

    refactor: modified db/migrate & setup_test_database files in line with DRY

--- db/migrate/01_setup_dev_db.rb
+++ db/migrate/01_setup_dev_db.rb
@@ -3,7 +3,10 @@
 # 1) psql
 # 2) CREATE DATABASE "seedlings_campaigns";
 require_relative '03_create_and_populate_tables'
+require_relative '04_populate_tables'
 
-p 'creating and populating the development database tables.....'
+p 'creating the development database tables.....'
+DatabaseCreation.create_tables('seedlings_campaigns')
 
-DatabaseCreation.create_and_populate_tables('seedlings_campaigns')
+p 'populating the development database tables.....'
+DatabasePopulation.populate_tables('seedlings_campaigns')
--- db/migrate/02_setup_test_db.rb
+++ db/migrate/02_setup_test_db.rb
@@ -3,7 +3,10 @@
 # 1) psql
 # 2) CREATE DATABASE "seedlings_campaigns_test";
 require_relative '03_create_and_populate_tables'
+require_relative '04_populate_tables'
 
-p 'creating and populating the test database tables.....'
+p 'creating the test database tables.....'
+DatabaseCreation.create_tables('seedlings_campaigns_test')
 
-DatabaseCreation.create_and_populate_tables('seedlings_campaigns_test')
+p 'populating test database tables.....'
+DatabasePopulation.populate_tables('seedlings_campaigns_test')
